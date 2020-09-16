2735fe5ec920d9f41c5b101df169505c3c4ad2c2
Ralph Sulit <ralphlsulit@gmail.com>
2020-09-16 17:47:26 +0800

    added navigation with span

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
 
-class App   extends Component {
+class App extends Component {
   render() {
     return (
       <div className='App'> 
+        <span style={{display: 'flex', justifyContent: 'flex-end', background: '#000'}} className="top-header f4 link dim pa2 pointer b gold">0-000-000</span>
+        <Navigation />
 
       </div>
     );
