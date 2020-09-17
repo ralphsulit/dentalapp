e1ac1e0f7d7fa4e65f11638357d1d7463d5e7d64
Ralph Sulit <ralphlsulit@gmail.com>
2020-09-17 21:52:00 +0800

    add navigation lists

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className='App'> 
-        <span style={{display: 'flex', justifyContent: 'flex-end', background: '#000'}} className="top-header f4 link dim pa2 pointer b gold">0-000-000</span>
+        <span style={{display: 'flex', justifyContent: 'flex-end', background: '#000'}} className="top-header f4 pa2 b gold">
+          <button href="#" className="pointer">0-000-000</button>
+        </span>
         <Navigation />
 
       </div>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,7 +3,23 @@ import React from 'react';
 const Navigation = () => {
   return(
     <nav>
-      
+      <div className="logo">
+        <h4>Logo</h4>
+      </div>
+      <ul>
+        <li>
+          <button className="link">Home</button>
+        </li>
+        <li>
+          <button className="link">Home</button>
+        </li>
+        <li>
+          <button className="link">Home</button>
+        </li>
+        <li>
+          <button className="link">Home</button>
+        </li>
+      </ul>
     </nav>
   )
 }
