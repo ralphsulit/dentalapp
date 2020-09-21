b79952035d4da67c0255c919d9687a817504d710
Ralph Sulit <ralphlsulit@gmail.com>
2020-09-21 22:44:24 +0800

    added hidden dropdown menu

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,25 +4,29 @@ import './Navigation.css'
 const Navigation = () => {
   return(
     <nav className="container">
-      <div className="logo">
+      <div className="logo pointer">
         <h4>Logo</h4>
       </div>
       <ul className="navbar">
         <li>
-          <button className="link nav-btn">Home</button>
+          <button className="link nav-btn pointer">Home</button>
+        </li>
+        <li className="dropmenu">
+          <button className="link nav-btn pointer">Home</button>
+          <div className="dropdown-content">
+            <ul>
+              <li><button className="link nav-btn">drop</button></li>
+              <li><button className="link nav-btn">drop</button></li>
+              <li><button className="link nav-btn">drop</button></li>
+              <li><button className="link nav-btn">drop</button></li>  
+            </ul>
+          </div>
         </li>
         <li>
-          <button className="link nav-btn">Home</button>
-          <ul className="dropmenu">
-            <li><button className="link nav-btn">drop</button></li>
-            <li><button className="link nav-btn">drop</button></li>
-          </ul>
+          <button className="link nav-btn pointer">Home</button>
         </li>
         <li>
-          <button className="link nav-btn">Home</button>
-        </li>
-        <li>
-          <button className="link nav-btn">Home</button>
+          <button className="link nav-btn pointer">Home</button>
         </li>
       </ul>
     </nav>
